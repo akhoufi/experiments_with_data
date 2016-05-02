c4048f65bf43f6049b2cfa285e98dd6bd7782f9f
akhoufi <a.khoufi@gmail.com>
2016-05-02 11:38:12 +0200

    Final submission

--- Model.R
+++ Model.R
@@ -29,6 +29,7 @@ IQR(train_cont$Age)
 library(gmodels)
 CrossTable(train$Sex, train$Income.Group)
 library(ggplot2)
+
 ggplot(train,aes(Sex,fill=Income.Group))+ geom_bar()+labs(title="Stacked Bar Chart",x="Sex",y="Count")+theme_bw()
 
 ggplot(train, aes(Sex,Hours.Per.Week))+geom_boxplot()+labs(title="Boxplot")
@@ -60,3 +61,24 @@ library(car)
 train$Workclass <- recode(train$Workclass, "c('State-gov','Self-emp-inc','Federal-gov','Without-pay','Never-worked')='Others'")
 test$Workclass <- recode(test$Workclass, "c('State-gov','Self-emp-inc','Federal-gov','Without-pay','Never-worked')='Others'")
 as.matrix(prop.table(table(train$Workclass)))
+
+#Predictive Modeling
+table(train$Income.Group)
+train$Income.Group<-ifelse(train$Income.Group=='<=50K',0,1)
+table(train$Income.Group)
+train<-subset(train,select=-c(ID))
+library(rpart)
+set.seed(333)
+train.tree<-rpart(Income.Group ~ ., data=train, method="class",control=rpart.control(minsplit=20,minbucket=100,maxdepth = 10),xval=5)
+
+summary(train.tree)
+library(rpart.plot)
+rpart.plot(train.tree)
+
+prediction_train<- predict(train.tree, newdata=train, type="class")
+prediction_test<-predict(train.tree,newdata = test,type="class")
+library(caret)
+confusionMatrix(prediction_train,train$Income.Group)
+solution_frame <- data.frame(ID=test$ID, Income.Group=prediction_test)
+solution_frame$Income.Group<-ifelse(solution_frame$Income.Group==0,'<=50K','>50K')
+write.csv(solution_frame,file='final_solution.csv')
